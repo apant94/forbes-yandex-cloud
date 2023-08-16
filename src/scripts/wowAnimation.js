f0e6767dd5b9659d2b9830b45cd97e13834d0060
Anna Panteleeva <apanteleeva94@gmail.com>
2023-08-16 17:00:43 +0300

    fix: edits animation for tranclateX

--- src/scripts/wowAnimation.js
+++ src/scripts/wowAnimation.js
@@ -1,6 +1,6 @@
 import WOW from 'wow.js';
 
-const wow = new WOW(
+const fadeUp = new WOW(
   {
     boxClass: 'animate',      // animated element css class (default is wow)
     // animateClass: 'animate__animated', // animation css class (default is animated)
@@ -12,4 +12,18 @@ const wow = new WOW(
     resetAnimation: true,     // reset animation on end (default is true)
   }
 );
-wow.init();
+fadeUp.init();
+
+const fadeRight = new WOW(
+  {
+    boxClass: 'animate__right',      // animated element css class (default is wow)
+    // animateClass: 'animate__animated', // animation css class (default is animated)
+    animateClass: 'animation__faderight',
+    offset: 0,          // distance to the element when triggering the animation (default is 0)
+    mobile: true,       // trigger animations on mobile devices (default is true)
+    live: true,       // act on asynchronously loaded content (default is true)
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+fadeRight.init();
