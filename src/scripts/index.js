a22c9b93f458a348531b53330bcd3fd3fe392720
Anna Panteleeva <apanteleeva94@gmail.com>
2023-08-18 12:55:13 +0300

    feat; animation of all the text

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import '../pages/index.css';
 import '../pages/article.css';
-// import './scrollAnimation.js'; анимация для главной страницы
+import './scrollAnimation.js'; //анимация для главной страницы
 import './wowAnimation.js';
 import './slider.js';
