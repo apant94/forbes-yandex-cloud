0ec503b21996db71c63b526c0780a789e6edf7ac
Anna Panteleeva <apanteleeva94@gmail.com>
2023-08-23 21:36:01 +0300

    fix: after speedrun insights analysis

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import '../pages/index.css';
 import '../pages/article.css';
 import './scrollAnimation.js'; //анимация для главной страницы
-import './wowAnimation.js';
-import './slider.js';
+import './wowAnimation.js'; // обрабатывает событие скролла контента
+import './slider.js'; // слайдер в статье о металлургии
