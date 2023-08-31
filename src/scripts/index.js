c9e698ae3c220cabb8a05d469ad915db6566eed0
Anna Panteleeva <apanteleeva94@gmail.com>
2023-08-31 14:45:27 +0300

    fix: efit svg tablet width

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,5 +3,4 @@ import '../pages/article.css';
 import './parallaxAnimation'; // анимация эффекта параллакса в иллюстрациях
 import './scrollAnimation.js'; //анимация для главной страницы
 import './wowAnimation.js'; // обрабатывает событие скролла контента
-// import './slider.js'; // слайдер в статье о металлургии
-import './scroller.js';
+import './scroller.js'; // обрабатывает горизонтальный скролл в металлургии
